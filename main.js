ae2a2b1fae78664e1737aa8a4c691d6075798333
anchalsinghrajput <anchalsingh9611@gmail.com>
2022-09-24 19:49:11 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const stickyNav = document.querySelector(".sticky-box-nav");
+
+stickyNav.addEventListener("click", (e) =>
+{
+    let navEle = e.target.closest(".sticky-box-nav-item");
+    navEle.classList.toggle("click-show");
+})
