2df2dd1f3087b25ca441269bf99bb5b2064ea5fe
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-09-24 23:42:51 +0530

    initial commit -m

--- main.js
+++ main.js
@@ -3,35 +3,31 @@
 document.addEventListener("click", (e) => {
   let navEle = e.target.closest(".sticky-box-nav-item");
   navEle.classList.toggle("click-show");
-  if(navEle.classList.contains("copy"))
-  {
-    const textContent = navEle.parent;
-    console.log(textContent)
-    textContent.select()
-    textContent.setSelectionRange(0,99)
-    document.execCommand('copy')
+  if (navEle.classList.contains("copy")) {
+    // const textContent = document.getElementById("text");
+    const textContent =
+      navEle.parentElement.parentElement.lastElementChild.firstElementChild;
+    console.log(textContent);
+    textContent.select();
+    textContent.setSelectionRange(0, 99);
+    document.execCommand("copy");
   }
 });
 
-document.addEventListener("input",(e)=>
-{
- const navEle = e.target.closest(".sticky-box-nav-item");
- if(navEle.classList.contains("color"))
- {
+document.addEventListener("input", (e) => {
+  const navEle = e.target.closest(".sticky-box-nav-item");
+  if (navEle.classList.contains("color")) {
     const colorValue = e.target.value;
     // console.log(colorValue);
-    document.getElementById("text").style.background =  colorValue;
-    document.getElementById("title").style.background =  colorValue;
-}
-
-
-})
+    document.getElementById("text").style.background = colorValue;
+    document.getElementById("title").style.background = colorValue;
+  }
+});
 
 // =========== create new note ===================
 
 const newNote = document.querySelector(".create-note");
 const container = document.querySelector(".container");
-console.log(newNote);
 newNote.addEventListener("click", (e) => {
   const createNewNote = `
         <div class="sticky-box">
@@ -53,6 +49,3 @@ newNote.addEventListener("click", (e) => {
     `;
   container.insertAdjacentHTML("beforeend", createNewNote);
 });
-
-
-
