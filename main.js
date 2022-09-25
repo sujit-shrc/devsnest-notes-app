362a5d12b494bde9c5be00e80776068580392535
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-09-25 13:09:17 +0530

    initial message

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // ============================= copy text functionality ===================================
-document.addEventListener("click", (e) => {
-  let navEle = e.target.closest(".sticky-box-nav-item");
-  if (navEle) {
-    navEle.classList.toggle("click-show");
-    if (navEle.classList.contains("copy")) {
-      const textContent = navEle.parentElement.parentElement.lastElementChild.firstElementChild;
-      console.log(textContent);
-      console.log(textContent.textContent);
-      // textContent.setSelectionRange(0, 99);
-      // document.execCommand("copy");
-    }
-  }
-});
+// document.addEventListener("click", (e) => {
+//   let navEle = e.target.closest(".sticky-box-nav-item");
+//   if (navEle) {
+//     navEle.classList.toggle("click-show");
+//     if (navEle.classList.contains("copy")) {
+//       const textContent = navEle.parentElement.parentElement.lastElementChild.firstElementChild;
+//       console.log(textContent);
+//       console.log(textContent.textContent);
+//       // textContent.setSelectionRange(0, 99);
+//       // document.execCommand("copy");
+//     }
+//   }
+// });
 
 // ==============================color changing functionality ==================================
 document.addEventListener("input", (e) => {
   const navEle = e.target.closest(".color");
   if (navEle) {
-    console.log(navEle);
-    const colorValue = navEle.firstElementChild.value;
+    const colorValue = navEle.value;
     navEle.parentElement.parentElement.lastElementChild.firstElementChild.style.background =
       colorValue;
     navEle.parentElement.nextElementSibling.firstElementChild.style.background =
@@ -59,27 +58,26 @@ newNote.addEventListener("click", (e) => {
 
 // ============================ Bold and Underline functionality ======================
 
-
-function bold(){
-  {       
-      var selection = window.getSelection().getRangeAt(0);
-      if(window.getSelection().baseNode.parentNode.id != "getText") return;
-      var selectedText = selection.extractContents();
-      var span = document.createElement("span");
-      span.classList.toggle("bold")
-      span.appendChild(selectedText);
-      selection.insertNode(span);
+function bold() {
+  {
+    var selection = window.getSelection().getRangeAt(0);
+    if (window.getSelection().baseNode.parentNode.id != "getText") return;
+    var selectedText = selection.extractContents();
+    var span = document.createElement("span");
+    span.classList.toggle("bold");
+    span.appendChild(selectedText);
+    selection.insertNode(span);
   }
 }
 
-function underline(){
-  {       
-      var selection = window.getSelection().getRangeAt(0);
-      if(window.getSelection().baseNode.parentNode.id != "getText") return;
-      var selectedText = selection.extractContents();
-      var span = document.createElement("span");
-      span.classList.toggle("underline")
-      span.appendChild(selectedText);
-      selection.insertNode(span);
+function underline() {
+  {
+    var selection = window.getSelection().getRangeAt(0);
+    if (window.getSelection().baseNode.parentNode.id != "getText") return;
+    var selectedText = selection.extractContents();
+    var span = document.createElement("span");
+    span.classList.toggle("underline");
+    span.appendChild(selectedText);
+    selection.insertNode(span);
   }
 }
